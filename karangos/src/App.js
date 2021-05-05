b305281f3c5fa354601cd7c77cd907ef41ec0ff6
Leviathan777-git <Gabre_346@hotmail.com>
2021-05-05 01:23:15 +0000

    (04/05) Término do layout básico e introdução do router

--- karangos/src/App.js
+++ karangos/src/App.js
@@ -1,5 +1,72 @@
 import './App.css';
-import TopBar from './ui/TopBar'
+import TopBar from './ui/TopBar';
+import FooterBar from './ui/FooterBar';
+import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import yellow from '@material-ui/core/colors/yellow';
+import pink from '@material-ui/core/colors/pink';
+import KarangosList from './routed/KarangosList';
+import KarangosForm from './routed/KarangosForm';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: yellow[500],
+    },
+    secondary: {
+      main: pink[500],
+    },
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  box: {
+    backgroundColor: theme.palette.background.default,
+    minHeight: '100vh',
+  },
+  routed: {
+    padding: '24px',
+    color: theme.palette.text.primary,
+    fontFamily: theme.typography.fontFamily,
+  }
+}));
+
+function Main() {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.box}>
+      <BrowserRouter>
+        <TopBar />
+        <Box id='routed' className={classes.routed}>
+          <Switch>
+            <Route path='/list'>
+              <KarangosList />
+            </Route>
+            <Route path='/new'>
+              <KarangosForm />
+            </Route>
+          </Switch>
+        </Box>
+        <FooterBar />
+      </BrowserRouter>
+    </Box>
+  )
+}
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Main />
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
 
 /*
     <div className="App">
@@ -20,29 +87,3 @@ import TopBar from './ui/TopBar'
         </a>
       </header>
     </div> */
-
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import yellow from '@material-ui/core/colors/yellow';
-import pink from '@material-ui/core/colors/pink';
-
-const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-    primary: {
-      main: yellow[500],
-    },
-    secondary: {
-      main: pink[500],
-    },
-  },
-});
-
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <TopBar />
-    </ThemeProvider>
-  );
-}
-
-export default App;
---
+++ karangos/src/ui/FooterBar.js
@@ -0,0 +1,40 @@
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import LocalCafeTwoToneIcon from '@material-ui/icons/LocalCafeTwoTone';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const useStyles = makeStyles((theme) => ({
+    text: {
+        //backgroundColor: "yellow", 
+        width: '100%',
+        color: theme.palette.text.secondary
+    },
+    toolbar: {
+        backgroundColor: theme.palette.background.paper,
+        minHeight: '42px',
+        //posicionando a barra no rodapé da pagina
+        width: '100%',
+        position: 'fixed',
+        bottom: 0, 
+    },
+    link: {
+        color: theme.palette.secondary.light,
+        textDecoration: 'none',
+        '&:hover': {
+            textDecoration: 'underline'
+        },
+    }
+}));
+
+export default function FooterBar() {
+    const classes = useStyles();
+
+    return (
+        <Toolbar className={classes.toolbar}>
+            <Typography variant="caption" align="center" className={classes.text}>
+                Desenvolvido com <LocalCafeTwoToneIcon fontSize='small' /> por <a href="gabriel.souza89@fatec.sp.gov.br	" className={classes.link}> Gabriel Desenvolvedor</a>
+            </Typography>
+        </Toolbar>
+    )
+
+}
